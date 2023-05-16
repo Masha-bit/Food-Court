b83f7cfef40a7ec30861964871f981ff22504a39
Masha-bit <mashachristopherifechukwude@gmail.com>
2023-05-16 11:40:23 +0100

    updated onswipe functions, add items functions

--- src/app/Cart/component/CartItems.jsx
+++ src/app/Cart/component/CartItems.jsx
@@ -2,61 +2,94 @@ import React, { useEffect, useState } from 'react'
 import {MdOutlineDelete} from 'react-icons/md'
 import {FiHeart} from 'react-icons/fi'
 import Image from 'next/image'
+import foodDes from 'react-icons/app/Menu/components/foodDes'
 
 
 export default function CartItems() {
 
-    const _INIT_VALUE = 1
-    const [numberOFItems, setNumberOFItems] = useState(_INIT_VALUE)
-    const [editIndex, seteditIndex] = useState(null)
-    const addItemsHandler = (id) => {
+    const items = foodDes.foodPlates
+    const cartItemsQuantity = items.map((item) => JSON.parse(`{"name": "${item.name}", "quantity": ${item.quantity}}`)) 
+    const [numberOFItems, setNumberOFItems] = useState(cartItemsQuantity)
+    const _INIT_VALUE = (itemName) => numberOFItems.map((item)=> {
+        if(item.name === itemName){
+            return item.quantity 
+        }
+        return null
+    })
+    
+    
+    
+    
+    const addItemsHandler = (itemName) => {  
+        const updateCartItemQuantity = numberOFItems.map((item)=> {
+            if(item.name === itemName){
+                return{...item, quantity: item.quantity + 1}
+            }
+            return item
+        })  
+        setNumberOFItems(updateCartItemQuantity)        
+        // console.log(cartItemsQuantity)
+    }
+    const subItemsHandler = (itemName) => {
+        const updateCartItemQuantity = numberOFItems.map((item)=> {
+            if(item.name === itemName){
+                if(item.quantity <= 1){
+                    removeCartItemHandler(itemName)
+                }
+                {
+                return{...item, quantity: item.quantity - 1}
+                }
+            }
+            return item
+        })  
+        setNumberOFItems(updateCartItemQuantity)        
+        // console.log(cartItemsQuantity)
+    }
 
 
-        seteditIndex(
-            editIndex => editIndex === id? null: id
-        )
-        setNumberOFItems(numberOFItems+1)
-    }
-    const subItemsHandler = (id) => {
-        setNumberOFItems(numberOFItems-1)
+    // handle if an element is clicked 
+
+    const [activeElement, setActiveElement] = useState(null)
+    const [swipeState, setSwipeState] = useState(false)
+    
+
+    const handleClick = (element) => {
+        setActiveElement(element)
+        setSwipeState(current => !current)
     }
 
-    const [isActive, setIsActive] = useState(false)
-    const swipeHandler = event =>{
-        console.log('swiped')
-        console.log(databaseFromLocalStorage.map((a)=> a.name).includes(undefined) || databaseFromLocalStorage.map((a)=> a.name).includes(''))
-        setIsActive(current => !current)
-    }
+    const isActive = (element) => {
+        return activeElement && activeElement.name === element.name;
+      };
+    //   setIsActive(current => !current)
+
+
+    // const swipeHandler = itemID =>{
+    //     console.log('swiped')
+    //     console.log(databaseFromLocalStorage.map((a)=> a.name).includes(undefined) || databaseFromLocalStorage.map((a)=> a.name).includes(''))
+    //     const clickedItem = cart.find((item) => item.name === itemID)
+    //     console.log(clickedItem)
+    //     setIsActive(current => !current)
+    // }
     
    
         const databaseFromLocalStorage =
-        //  useEffect(()=>{
             typeof window !== "undefined"?
-            JSON.parse(
+            JSON.
+            parse(
             localStorage.getItem('Cart') || `[{}]` 
             ):null
-    // })
+
     console.log(typeof window !== undefined)
-    // console.log( JSON.parse(
-    //     `[{}]` || localStorage.getItem('Cart') 
-    //     ))
 
-    const [cart, setCart] = useState(
-        databaseFromLocalStorage
-    )
+    const [cart, setCart] = useState(databaseFromLocalStorage)
     console.log(databaseFromLocalStorage)
-
-    const a = databaseFromLocalStorage.map((b) => b)
-    const [z, setZ] = useState(
-        a
-    )
 
     function removeCartItemHandler(name){
         const remainingCart = cart.filter((cartItem) => name !== cartItem.name)
-
         console.log(`deleted`)
         console.log(remainingCart)
-      
+        window.alert(`${name} is being removed from cart`)      
 
 
         const index = databaseFromLocalStorage.findIndex(product => product.name === name)
@@ -64,15 +97,6 @@ export default function CartItems() {
         const y = databaseFromLocalStorage.splice(index, 1)
         setCart(y)
         }console.log('not removed')
-        // console.log(z)
-        // console.log(index)
-
-
-        // const updatelocalstorageName = a.filter((valuename)=> valuename == name )
-
-
-        // a.includes(a[a.name])? console.log(a.filter((valuename)=> valuename != a)): console.log('blah')
-        // console.log(a.filter((valuename)=> valuename == name ))
         setCart(remainingCart)
     }
  
@@ -83,20 +107,20 @@ export default function CartItems() {
     cart.map((cartItem) =>
     (
         <div  key={cartItem.name} className={`h-[auto] w-[100%] rounded-normal flex flex-col items-center  font-dongle p-2 relative ${cartItem.slug == '' || cartItem.slug == undefined? `hidden`: ``} `}>
-        <div className={`h-[95px] w-[100%] rounded-normal flex items-center  bg-white font-dongle my-2 p-2 z-[2] shadow-2xl ${isActive? `text-slate-600 -translate-x-[9rem] transition-all`:`transition-all`} `}>
+        <div className={`h-[95px] w-[100%] rounded-normal flex items-center  bg-white font-dongle my-2 p-2 z-[2] shadow-2xl ${isActive(cartItem) && swipeState? `text-slate-600 -translate-x-[9rem] transition-all`:`transition-all`} `}>
           <div className='h-[90px] w-[90px] rounded-full bg-slate-100'>
             <Image src={cartItem.url} alt={cartItem.slug} height={90} width={90}/>
         </div>
   
           <div>
               <div className=' font-bold mx-2 p-2'>
-                  <p className='text-[20px]'  onClick={swipeHandler}>{cartItem.name}</p>
+                  <p className='text-[20px]'  onClick={() => handleClick(cartItem)}>{cartItem.name}</p>
                   <div className='text-[14px] flex items-center justify-between w-[250px]'>
                       <p>₦ {cartItem.price}</p>
                       <div id={cartItem.slug} className='h-[40px] w-[80px] rounded-full bg-[#FFC83A] flex items-center justify-around' >
-                                <button onClick={subItemsHandler}>-</button>
-                                <p>{numberOFItems}</p> 
-                                <button onClick={addItemsHandler}>+</button>
+                                <button onClick={()=>subItemsHandler(cartItem.name)}>-</button>
+                                <p>{_INIT_VALUE(cartItem.name)}</p> 
+                                <button onClick={()=>addItemsHandler(cartItem.name)}>+</button>
                       </div>
                   </div>
                   
