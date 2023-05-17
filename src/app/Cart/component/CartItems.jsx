74dfedbb9888e41495a2b949be6b6ecea4d05a82
Masha-bit <mashachristopherifechukwude@gmail.com>
2023-05-17 01:44:29 +0100

    updated cart total function and created local storage for favorite items

--- src/app/Cart/component/CartItems.jsx
+++ src/app/Cart/component/CartItems.jsx
@@ -16,13 +16,21 @@ export default function CartItems() {
         }
         return null
     })
-    
+
+    const _Q = (itemName) => {
+        return _INIT_VALUE(itemName).filter((x)=> x != null)[0]
+    } 
+    const updatedPrice = (cartItem)=>{
+        return cartItem.price*_Q(cartItem.name)
+    }   
     
     
     
     const addItemsHandler = (itemName) => {  
         const updateCartItemQuantity = numberOFItems.map((item)=> {
             if(item.name === itemName){
+                const l = _Q(itemName)
+                console.log(l)
                 return{...item, quantity: item.quantity + 1}
             }
             return item
@@ -72,32 +80,100 @@ export default function CartItems() {
     //     setIsActive(current => !current)
     // }
     
-   
+   useEffect(() => {
         const databaseFromLocalStorage =
             typeof window !== "undefined"?
             JSON.
             parse(
             localStorage.getItem('Cart') || `[{}]` 
             ):null
+        databaseFromLocalStorage? setCart(databaseFromLocalStorage): null
+        
+        
+        }, []);
 
     console.log(typeof window !== undefined)
 
-    const [cart, setCart] = useState(databaseFromLocalStorage)
-    console.log(databaseFromLocalStorage)
+    const favoriteDatabaseFromLocalStorage =
+//  useEffect(()=>{
+    typeof window !== undefined?
+    JSON.parse(  localStorage.getItem('Favorite') 
+    ): null
+// })
+
+    const [cart, setCart] = useState([])
+    const product = (name) =>{
+     return cart.find(a => a.name === name)
+    }
+    const [favorite, setfavorite] = useState(
+        favoriteDatabaseFromLocalStorage
+        // [product]
+    )    
+    
+
+
+    useEffect(() => {
+        if(window !== 'undefined'){
+        localStorage.setItem('Favorite', JSON.stringify(favorite))         
+    ,[favorite]
+}})
+
+
+    const [LikeButton, setLikeButton] = useState(
+        LikeIcon
+    )
+    function LikeIcon(){
+        return (
+            <div>
+                <FiHeart size={30}/>
+            </div>
+        )
+    }
+    function Liked(){
+        return (
+            <div>
+                <FiHeart size={30} style={{fill:'black'}}/>
+            </div>
+        )
+    }
+    function NotLiked(){
+        return (
+            <div>
+                <FiHeart size={30} style={{color:'black'}}/>
+            </div>
+        )
+    }
+    function addToFavorite(name){
+        console.log(favoriteDatabaseFromLocalStorage)
+        if(!favorite.map(a => a.name).includes(product(name).name)){
+            setLikeButton(Liked)
+            setfavorite([
+                ...favorite,
+                product(name)
+            ])
+            console.log(`true`)
+        
+        }else{     
+         const remainingFavorite = favorite.filter((cartItem) => name !== cartItem.name)
+         setLikeButton(NotLiked)
+         setfavorite(remainingFavorite)
+        console.log(`false`)
+            }
+    } 
+    
+    
 
     function removeCartItemHandler(name){
         const remainingCart = cart.filter((cartItem) => name !== cartItem.name)
         console.log(`deleted`)
         console.log(remainingCart)
         window.alert(`${name} is being removed from cart`)      
-
-
-        const index = databaseFromLocalStorage.findIndex(product => product.name === name)
-        if(index > -1){
-        const y = databaseFromLocalStorage.splice(index, 1)
-        setCart(y)
-        }console.log('not removed')
-        setCart(remainingCart)
+        setCart(remainingCart);
+        typeof window !== "undefined"?
+            JSON.
+            parse(
+            localStorage.setItem('Cart', JSON.stringify(remainingCart))  || `[{}]`
+            ):null
     }
  
 
@@ -107,7 +183,7 @@ export default function CartItems() {
     cart.map((cartItem) =>
     (
         <div  key={cartItem.name} className={`h-[auto] w-[100%] rounded-normal flex flex-col items-center  font-dongle p-2 relative ${cartItem.slug == '' || cartItem.slug == undefined? `hidden`: ``} `}>
-        <div className={`h-[95px] w-[100%] rounded-normal flex items-center  bg-white font-dongle my-2 p-2 z-[2] shadow-2xl ${isActive(cartItem) && swipeState? `text-slate-600 -translate-x-[9rem] transition-all`:`transition-all`} `}>
+        <div className={`h-[auto] w-[100%] rounded-normal flex items-center  bg-white font-dongle my-2 p-2 z-[2] shadow-2xl ${isActive(cartItem) && swipeState? `text-slate-600 -translate-x-[9rem] transition-all`:`transition-all`} `}>
           <div className='h-[90px] w-[90px] rounded-full bg-slate-100'>
             <Image src={cartItem.url} alt={cartItem.slug} height={90} width={90}/>
         </div>
@@ -116,10 +192,10 @@ export default function CartItems() {
               <div className=' font-bold mx-2 p-2'>
                   <p className='text-[20px]'  onClick={() => handleClick(cartItem)}>{cartItem.name}</p>
                   <div className='text-[14px] flex items-center justify-between w-[250px]'>
-                      <p>₦ {cartItem.price}</p>
+                      <p>₦ {updatedPrice(cartItem)}</p>
                       <div id={cartItem.slug} className='h-[40px] w-[80px] rounded-full bg-[#FFC83A] flex items-center justify-around' >
                                 <button onClick={()=>subItemsHandler(cartItem.name)}>-</button>
-                                <p>{_INIT_VALUE(cartItem.name)}</p> 
+                                <p>{_Q(cartItem.name)}</p> 
                                 <button onClick={()=>addItemsHandler(cartItem.name)}>+</button>
                       </div>
                   </div>
@@ -131,21 +207,33 @@ export default function CartItems() {
           <div className='h-[50px] w-[50px] rounded-full bg-[#FFC83A] flex items-center justify-center'
             onClick={() => removeCartItemHandler(cartItem.name)}
           ><MdOutlineDelete size={30}/></div>
-          <div className='h-[50px] w-[50px] rounded-full bg-[#FFC83A] flex items-center justify-center'> <FiHeart size={30}/> </div>
+          <div className='h-[50px] w-[50px] rounded-full bg-[#FFC83A] flex items-center justify-center' onClick={()=> addToFavorite(cartItem.name)}> 
+          {LikeButton}
+          </div>
       </div>
       </div>
     )):
     (
-        <div>Not rendereing localStorage</div>
+        <div className={`h-[auto] w-[100%] rounded-normal flex flex-col items-center  font-dongle p-2 relative`}>Not rendereing localStorage</div>
     )
-    console.log(databaseFromLocalStorage)
+  
+    const cartPrices= cart.map((cartItem) => updatedPrice(cartItem)).filter(Boolean)
+    const totalCartPrice = cartPrices != 0?cartPrices.filter((item)=> typeof item == 'number').reduce((a,b) => a + b): 0
+    
+        // ?cart.map((cartItem) => cartItem.price)      
+        // .reduce((a,b) => a + b): console.log(`e no work`)
+    // }
+    console.log(totalCartPrice)
+
 
     
 
 
   return (
-    <div>
+    <div className=''>
         {cartItems}
+        
+        <div className={`h-[auto] w-[100%] rounded-normal flex flex-col items-center  font-dongle p-2 absolute bottom-0 left-0 text-[25px] bg-slate-50`}>{`SubTotal : ₦${totalCartPrice}`}</div> 
     </div>
   )
 }
